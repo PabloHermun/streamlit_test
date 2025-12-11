076becce10b38936402d7edf61d6283fd37eb967
PabloHermun <pablo_hermun@ciencias.unam.mx>
2025-12-11 02:05:44 -0600

    Added a slider and execute button.

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header("Flip a coin")
 
+number_of_trials = st.slider('¿Number of throws?', 1, 1000, 10)
+start_button = st.button('Execute')
+
+if start_button:
+    st.write(f'Experiment with {number_of_trials} trials in progress')
+
 st.write("This app is under construction.")
